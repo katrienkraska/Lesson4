f4604a1c3937a516000a1cf04ab9ea7cd068232d
Катерина <kate2002.kk42@gmail.com>
2023-10-14 17:11:18 +0300

    finish task

--- src/task5/ConverterTemperature.java
+++ src/task5/ConverterTemperature.java
@@ -1,23 +1,27 @@
 package task5;
 
 public class ConverterTemperature {
-    public static double celsiusToKelvin(double celsius) {
-        // Формула для конвертації з Цельсієм в Кельвіни: K = C + 273.15
-        return celsius + 273.15;
+    public static double convert (double celsius, String targetUnit) {
+        double result = 0.0;
+
+        if (targetUnit == "Kelvin"){
+            // Формула для конвертації з Цельсієм в Кельвіни: K = C + 273.15
+            result = celsius + 273.15;
+        } else if (targetUnit == "Fahrenheit"){
+            // Формула для конвертації з Цельсієм в Фаренгейти: F = (C × 9/5) + 32
+            result = (celsius * 9 / 5) + 32;
+        }
+        return result;
     }
 
-    public static double celsiusToFahrenheit(double celsius) {
-        // Формула для конвертації з Цельсієм в Фаренгейти: F = (C × 9/5) + 32
-        return (celsius * 9 / 5) + 32;
-    }
     public static void main(String[] args) {
         double celsius = 22.0;
 
-        double kelvin = celsiusToKelvin(celsius);
+        double kelvin = convert(celsius, "Kelvin");
         System.out.println(celsius + " градусів Цельсія = " + kelvin + " Келвін");
         System.out.println();
 
-        double fahrenheit = celsiusToFahrenheit(celsius);
+        double fahrenheit = convert(celsius, "Fahrenheit");
         System.out.println(celsius + " градусів Цельсія = " + fahrenheit + " Фаренгейт");
         System.out.println();
     }
